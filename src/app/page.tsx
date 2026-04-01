59d289f06cda24f4d1b6529230ede3570a6a40a2
Xander-Tosnim <tosnimila69@gmail.com>
2026-04-02 05:26:56 +0600

    code basic features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,104 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  
+  type Player = "X" | "O" | null;
+
+  const [ board   , setBoard   ] = useState<Player[]>(Array(9).fill(null));
+  const [ isXTurn , setIsXTurn ] = useState<boolean>(true);
+
+  const toWinCombination: number[][] = [
+    // for horizontal
+    [ 0 , 1 , 2 ],
+    [ 3 , 4 , 5 ],
+    [ 6 , 7 , 8 ],
+    // for vertical
+    [ 0 , 3 , 6 ],
+    [ 1 , 4 , 7 ],
+    [ 2 , 5 , 8 ],
+    // for cross-ways
+    [ 0 , 4 , 8 ],
+    [ 2 , 4 , 6 ]
+  ]
+
+  const getWinner = (squares: Player[]) => {
+    for(let combination of toWinCombination) {
+      const [ a , b , c ] = combination;
+
+      if(
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c] 
+      ) return squares[a]
+    }
+
+    return null;
+  }
+
+  const handleSquareClick = (i: number): void => {
+    if( board[i] || getWinner(board) ) return;
+
+    const updatedBoard = [...board];
+    updatedBoard[i] = isXTurn ? "X" : "O";
+
+    setBoard(updatedBoard);
+    setIsXTurn(!isXTurn); 
+  }
+
+  const resetGame = ():void => {
+    setBoard(Array(9).fill(null));
+    setIsXTurn(true);
+  }
+
+  const getGameStatus = (): string => {
+    const winner = getWinner(board);
+
+    if(winner) return `Winner: ${winner}`;
+
+    if(board.every((square) => square !== null)) return "It's a Draw!"
+
+    return `Turn For Player: ${isXTurn ? "X" : "O"}`;
+  }
+  
+  useEffect(() => {
+    const winner = getWinner(board);
+    const draw = board.every((square) => square !== null);
+
+    if( winner || draw ) setTimeout(() => { resetGame() }, 2000) ;
+
+  }, [board]);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
+
+      <div className="w-full max-w-md mx-5">
+        <h1 className="text-5xl font-semibold text-white mb-8 text-center">TIC TAC TOE</h1>
+
+        <div className={`text-center mb-6 ${getWinner(board) ? "text-2xl font-bold text-green-400 animate-bounce" : "text-xl text-white"}`}>
+          {getGameStatus()}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="grid grid-cols-3 gap-1 rounded-xl overflow-hidden mb-6">
+          {board.map(( square , index ) => (
+            <button 
+              key={index} 
+              onClick={() => handleSquareClick(index)} 
+              className={`h-32 w-full bg-gray-800 rounded-md text-6xl font-light transition-colors duration-200 hover:bg-gray-700 ${square === "X" ? "text-white" : "text-slate-400"}`}
+            >
+              {square}
+            </button>
+          ))}
         </div>
-      </main>
+
+        <button 
+          className="w-full py-3 text-lg text-white border rounded-xl hover:bg-gray-50 hover:text-gray-800 transition-colors duration-200"
+          onClick={() => resetGame()}
+        >
+          Restart
+        </button>
+      </div>
     </div>
   );
 }
